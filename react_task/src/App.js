14768ae33bd6a2dca9849ed00ddf239bce9ede9b
MSurendra <2200030604@kluniversity.in>
2025-03-14 14:34:32 +0530

    iniital commit

---
+++ react_task/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Head from 
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1>Welcome Surendra</h1>
+      
+    </div>
+  );
+}
+
+export default App;   
